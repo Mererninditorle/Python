a46de51047b5053c05be84c3930da4a824da9834
= <ivanshapovalov787@gmail.com>
2021-09-13 13:54:43 +0300

    Python_Web

---
+++ 6.9.21/main.py
@@ -0,0 +1,18 @@
+# print ("Hello world!")
+# type("A")
+# #comment
+
+# user = input("What's your name?")
+# print("Hello, " + user)
+# #или
+# print(f"Hello, {user}")
+
+
+num1 = input("Напишите первое число:")
+num2 = input("Напишите второе число:")
+if num1.isdigit() and num2.isdigit():
+    int(num3) = num1 + num2
+    print("Результат: " + int(num1) + " + " + int(num2) + " = " + int(num3))
+else:
+    print("Enter digits")
+    
